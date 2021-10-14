c79499d2ba7dd67689911b70665cad3d1d19f409
turningpoint1988 <35712510+turningpoint1988@users.noreply.github.com>
2021-10-14 19:18:17 +0800

    Update FCNmotif.py

--- FCNmotif.py
+++ FCNmotif.py
@@ -19,7 +19,7 @@ def bn_relu_conv(in_, out_, kernel_size=3, stride=1, bias=False, groups=1):
 
 
 class FCNA(nn.Module):
-    """FCNA for motif mining"""
+    """FCNA"""
     def __init__(self, motiflen=13):
         super(FCNA, self).__init__()
         # encode process
@@ -90,17 +90,17 @@ class FCNA(nn.Module):
 
 
 class FCNsignal(nn.Module):
-    """FCNsignal for motif mining"""
+    """FCNsignal for predicting base-resolution binding signals"""
     def __init__(self, motiflen=16):
         super(FCNsignal, self).__init__()
-        # encode process
+        # encoding
         self.conv1 = nn.Conv1d(in_channels=4, out_channels=64, kernel_size=motiflen)
         self.pool1 = nn.MaxPool1d(kernel_size=4, stride=4)
         self.conv2 = nn.Conv1d(in_channels=64, out_channels=64, kernel_size=5)
         self.pool2 = nn.MaxPool1d(kernel_size=4, stride=4)
         self.conv3 = nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3)
         self.pool3 = nn.MaxPool1d(kernel_size=4, stride=4)
-        # decode process
+        # decoding
         self.gru1 = nn.GRU(input_size=64, hidden_size=64, batch_first=True)
         self.gru2 = nn.GRU(input_size=64, hidden_size=64, batch_first=True)
         self.gru_drop = nn.Dropout(p=0.2)
@@ -129,7 +129,7 @@ class FCNsignal(nn.Module):
     def forward(self, data):
         """Construct a new computation graph at each froward"""
         b, _, _ = data.size()
-        # encode process
+        # encoding
         skip1 = data
         out1 = self.conv1(data)
         out1 = self.relu(out1)
@@ -152,7 +152,7 @@ class FCNsignal(nn.Module):
         out1 = self.gru_drop(out1)
         skip4 = out1.permute(0, 2, 1)
         up5 = self.aap(skip4)
-        # decode process
+        # decoding
         up4 = upsample(up5, skip4.size()[-1])
         up4 = up4 + skip4
         up4 = self.blend4(up4)
@@ -218,17 +218,17 @@ class BPNet(nn.Module):
 
 
 class FCNsignalW(nn.Module):
-    """FCNsignalW for motif mining"""
+    """FCNsignalW for predicting base-resolution binding signals (kernel number = 128)"""
     def __init__(self, motiflen=16):
         super(FCNsignalW, self).__init__()
-        # encode process
+        # encoding
         self.conv1 = nn.Conv1d(in_channels=4, out_channels=128, kernel_size=motiflen)
         self.pool1 = nn.MaxPool1d(kernel_size=4, stride=4)
         self.conv2 = nn.Conv1d(in_channels=128, out_channels=64, kernel_size=5)
         self.pool2 = nn.MaxPool1d(kernel_size=4, stride=4)
         self.conv3 = nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3)
         self.pool3 = nn.MaxPool1d(kernel_size=4, stride=4)
-        # decode process
+        # decoding
         self.gru1 = nn.GRU(input_size=64, hidden_size=64, batch_first=True)
         self.gru2 = nn.GRU(input_size=64, hidden_size=64, batch_first=True)
         self.gru_drop = nn.Dropout(p=0.2)
@@ -257,7 +257,7 @@ class FCNsignalW(nn.Module):
     def forward(self, data):
         """Construct a new computation graph at each froward"""
         b, _, _ = data.size()
-        # encode process
+        # encoding
         skip1 = data
         out1 = self.conv1(data)
         out1 = self.relu(out1)
@@ -280,7 +280,7 @@ class FCNsignalW(nn.Module):
         out1 = self.gru_drop(out1)
         skip4 = out1.permute(0, 2, 1)
         up5 = self.aap(skip4)
-        # decode process
+        # decoding
         up4 = upsample(up5, skip4.size()[-1])
         up4 = up4 + skip4
         up4 = self.blend4(up4)
